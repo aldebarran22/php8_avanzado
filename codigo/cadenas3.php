523a116f4b45cd4b416ab656b248775acc9dc453
aldebarran22 <antonioespinherranz@hotmail.com>
2025-10-20 10:57:56 +0200

    65

--- codigo/cadenas3.php
+++ codigo/cadenas3.php
@@ -1,4 +1,9 @@
 <?php
+function sanitizacion(string $cadena):string {
+    // Funcion que limpia los caracteres especiales.
+    return htmlspecialchars($cadena, ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8');
+}
+
 echo "<p>qs:".$_SERVER['QUERY_STRING']."</p>";
 
 if (strcmp($_SERVER['QUERY_STRING'],'')==0)
@@ -9,5 +14,13 @@ else {
     $arr = [];
     parse_str($qs, $arr);
     echo $arr['nombre'] . " " . $arr['curso'];
+
+    echo "type de qs: ".gettype($qs)."<br>";
+    echo "type de arr: ".gettype($arr)."<br>";
+
 }
+
+$cad = "<script>alert('hola')</script>";
+echo $cad; // ojo interpreta
+echo sanitizacion($cad); // lo deja como una cadena
 ?>
