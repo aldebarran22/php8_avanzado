dbf884a4253336f39ffb9a02bdc0f8235967a140
aldebarran22 <antonioespinherranz@hotmail.com>
2025-10-21 10:46:43 +0200

    34

--- codigo/subir_ficheros/recibir.php
+++ codigo/subir_ficheros/recibir.php
@@ -8,4 +8,14 @@
         }
         echo "<hr>";
     }
+
+    // Para grabar los ficheros:
+    foreach ($_FILES as $k => $info_file){
+        if (is_uploaded_file($_FILES[$k]['tmp_name'])){
+            // Si ha subido correctamente:
+            
+            $path = $_SERVER['DOCUMENT_ROOT'] . "/curso_php8/almacen/" . $_FILES[$k]['name'];
+            move_uploaded_file($_FILES[$k]['tmp_name'], $path);
+        }
+    }
 ?>
