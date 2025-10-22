6be20625e48cf54ed19a2a989f9d63e1b6bfd35e
aldebarran22 <antonioespinherranz@hotmail.com>
2025-10-22 09:59:41 +0200

    ee

--- codigo/herencia.php
+++ codigo/herencia.php
@@ -1,9 +1,9 @@
 <?php
 
   class Persona {
-	private string $dni;
-	private string $nombre;
-	private string $apellidos;
+	protected string $dni;
+	protected string $nombre;
+	protected string $apellidos;
 
 	function __construct(string $dni, string $nombre, string $apellidos){
 	  $this->nombre=$nombre;
@@ -11,8 +11,41 @@
       $this->dni=$dni;
 	}
 
+   
+
 	function __toString():string{
 	  return $this->nombre . ", " . $this->apellidos . " " . $this->dni;
 	}
   }
+
+  class Empleado extends Persona {
+
+    private string $empresa;
+    private float $sueldo;
+
+    public function __construct(string $dni, string $nombre, 
+    string $apellidos, string $empresa, float $sueldo){
+        // Llamar al constructor de la clase Padre:
+        parent::__construct($dni, $nombre, $apellidos);
+
+        // Almacenar los att de la clase:
+        $this->empresa = $empresa;
+        $this->sueldo = $sueldo;
+    }
+
+    function modificarDni($dni){
+        $this->dni = $dni;
+    }
+
+    function __toString():string{
+        return parent::__toString() . " " . $this->empresa . " " . $this->sueldo;
+    }
+  }
+
+  // Crear un empleado:
+  $emp = new Empleado("1234567D","Jorge", "Sanz", "TRT", 2000.0);
+  echo $emp . "<br>";
+  //$emp->dni = "34343434Y";
+  $emp->modificarDni("22334455H");
+  echo $emp . "<br>";
 ?>
