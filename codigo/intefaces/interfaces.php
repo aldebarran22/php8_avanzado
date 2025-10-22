5f62b78180604b70d9ec482a9914757c06b2c3b6
aldebarran22 <antonioespinherranz@hotmail.com>
2025-10-22 13:34:12 +0200

    23

--- codigo/intefaces/interfaces.php
+++ codigo/intefaces/interfaces.php
@@ -1,40 +1,72 @@
 <?php
+require_once __DIR__ . "/ProductoCSV.php";
+
 interface Operaciones {
 
-    function grabar(string $path):void;
-    function cargar(string $path):array;
+    //function grabar(string $path):void;
+    function cargar():array;
 }
 
 class Ficheros implements Operaciones {
 
-    function grabar(string $path):void{
-         echo "grabar a fichero<br>";
+    private string $path;
+        
+    function __construct(string $path){
+        $this->path = $path;
     }
-    function cargar(string $path):array{
-        echo "cargar de fichero<br>";
-        return array();
+
+    function cargar():array{
+        return ProductoCSV::load($this->path);
     }
 }
 
 class BaseDatos implements Operaciones {
 
-    function grabar(string $path):void{
-        echo "grabar a BD<br>";
+    private PDO $pdo;
+
+    function __construct($host, $bd, $user, $pwd){
+        try {
+            $pdo = new PDO("mysql:host=$host;dbname=$$bd;charset=utf8mb4", $user, $pwd, [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // lanza excepciones en errores
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC // devuelve arrays asociativos
+            ]);
+            $this->pdo = $pdo;
+          
+
+        } catch (PDOException $e) {
+            throw $e;
+        }
     }
-
     
-    function cargar(string $path):array{
-         echo "cargar de BD<br>";
-        return array(); 
+    function cargar():array {
+        $productos = array();
+        $sql = "SELECT p.id as idp, p.nombre as nombrep, " .
+        " c.id as idc, c.nombre as nombrec, p.precio, p.existencias " .
+        " FROM productos p inner join categorias c " .
+        " on c.id = p.idcategoria";
 
+        // Crear la sentencia con el SQL
+        $stmt = $this->pdo->prepare($sql);
+
+        // Ejecutar el sql
+        $stmt->execute();
+
+        // Recuperar los resultados:
+        $resultados = $stmt->fetchAll();
+        foreach ($resultados as $resul) {
+            $producto = Producto::create($resul);
+            $productos[] = $producto;
+        }
+        return $productos;
     }
 }
 
 function operar(Operaciones $op){
-    $datos = $op->cargar("datos.csv");
-    $op->grabar("datos2.csv");
+    $productos = $op->cargar();
+    echo "Productos: " . count($productos);
+
 }
 
-operar(new Ficheros());
-operar(new BaseDatos());
+operar(new Ficheros("productos.csv"));
+//operar(new BaseDatos("localhost","empresa3","root","antonio"));
 ?>
