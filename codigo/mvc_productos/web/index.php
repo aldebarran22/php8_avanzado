d87406fb2a8c38e5c1e437f9d3076c2e77ce15ec
aldebarran22 <antonioespinherranz@hotmail.com>
2025-10-24 10:06:52 +0200

    54

--- codigo/mvc_productos/web/index.php
+++ codigo/mvc_productos/web/index.php
@@ -1,16 +1,47 @@
 <?php
  // carga del modelo y los controladores
  require_once __DIR__ . '/../app/Config.php';
- require_once __DIR__ . '/../app/Model.php';
- require_once __DIR__ . '/../app/Controller.php';
+ require_once __DIR__ . '/../app/modelo/dao/ProductoDAO.php';
+ require_once __DIR__ . '/../app/Controlador.php';
  
 
  // enrutamiento
  $map = array(
      'inicio' => array('controller' =>'Controller', 'action' =>'inicio'),
-     'listar' => array('controller' =>'Controller', 'action' =>'listar'),
-     'insertar' => array('controller' =>'Controller', 'action' =>'insertar'),
-     'buscar' => array('controller' =>'Controller', 'action' =>'buscarPorNombre'),
-     'ver' => array('controller' =>'Controller', 'action' =>'ver')
+     'nuevoproducto' => array('controller' =>'Controller', 'action' =>'nuevoProducto'),
+     'grabarproducto' => array('controller' =>'Controller', 'action' =>'grabarProducto'),
+     'buscarproductos' => array('controller' =>'Controller', 'action' =>'buscadorProductos'),
+     'listarproductos' => array('controller' =>'Controller', 'action' =>'listarProductos')
  );
+
+ // Analizar la ruta:
+ if (isset($_GET['action'])) {
+     if (isset($map[$_GET['action']])) {
+         $ruta = $_GET['action'];
+
+     } else {
+         header('Status: 404 Not Found');
+         echo '<html><body><h1>Error 404: No existe la ruta <i>' .
+                 $_GET['action'] .
+                 '</p></body></html>';
+         exit;
+     }
+ } else {
+     $ruta = 'inicio';
+ }
+
+  $controlador = $map[$ruta];
+ // Ejecucion del controlador asociado a la ruta
+
+ if (method_exists($controlador['controller'],$controlador['action'])) {
+     call_user_func(array(new $controlador['controller'], $controlador['action']));
+ } else {
+
+     header('Status: 404 Not Found');
+     echo '<html><body><h1>Error 404: El controlador <i>' .
+             $controlador['controller'] .
+             '->' .
+             $controlador['action'] .
+             '</i> no existe</h1></body></html>';
+ }
  ?>
